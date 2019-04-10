6d1269e4f05cd5772126acd13099d6413b333acd
Herna Michel <labellehernamichel@yahoo.fr>
2019-04-10 18:30:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,8 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   my_hash = {
-    "my_name" => "sean",
-    "my_height" => "9 feet 10 inches",
+    "my_name" => "Peter",
+    "gender" => "9 feet 10 inches",
     "my_weight" => "300 Kilograms",
   }
 
